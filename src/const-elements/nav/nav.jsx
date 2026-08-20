87692e7b29f73b8e6e59be2b8b88bfcda7484ef6
orangedragon369-crypto <orangedragon369@gmail.com>
2026-08-20 15:21:26 -0600

    fix nav

--- src/const-elements/nav/nav.jsx
+++ src/const-elements/nav/nav.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from "react-router-dom";
 
-export default function Nav({baseUrl}){
+export default function Nav(){
   const activeClass = ({ isActive }) => isActive ? "active" : "";
 
   return (
     <nav className="column">
-      <NavLink to={baseUrl + "/"}end className={activeClass}>Home</NavLink>
-      <NavLink to={baseUrl + "/how-it-works"} className={activeClass}>How It Works</NavLink>
-      <NavLink to={baseUrl + "/douglas-ludington"} className={activeClass}>Douglas Ludington, NP</NavLink>
-      <NavLink to={baseUrl + "/services"} className={activeClass}>Services</NavLink>
-      <NavLink to={baseUrl + "/rates-and-pricing"} className={activeClass}>Rates & Pricing</NavLink>
-      <NavLink to={baseUrl + "/faqs"} className={activeClass}>FAQs</NavLink>
-      <NavLink to={baseUrl + "/contact"} className={activeClass}>Contact</NavLink>
-      <NavLink to={baseUrl + "/get-on-the-waitlist"} className={activeClass}>Get on the Waitlist</NavLink>
-      <NavLink to={baseUrl + "/calendar"} className={activeClass}>calendar test link</NavLink>
+      <NavLink to={"/"}end className={activeClass}>Home</NavLink>
+      <NavLink to={"/how-it-works"} className={activeClass}>How It Works</NavLink>
+      <NavLink to={"/douglas-ludington"} className={activeClass}>Douglas Ludington, NP</NavLink>
+      <NavLink to={"/services"} className={activeClass}>Services</NavLink>
+      <NavLink to={"/rates-and-pricing"} className={activeClass}>Rates & Pricing</NavLink>
+      <NavLink to={"/faqs"} className={activeClass}>FAQs</NavLink>
+      <NavLink to={"/contact"} className={activeClass}>Contact</NavLink>
+      <NavLink to={"/get-on-the-waitlist"} className={activeClass}>Get on the Waitlist</NavLink>
+      <NavLink to={"/calendar"} className={activeClass}>calendar test link</NavLink>
     </nav>
   );
 }
